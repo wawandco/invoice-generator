35860042fd02a6dcd307dac92a46b9084f30b3b0
Bryan Moslo <balgutria@wawand.co>
2024-09-17 16:51:58 -0500

    adding an endpoint description

--- api/invoice.go
+++ api/invoice.go
@@ -7,6 +7,8 @@ import (
 	"encore.app/model"
 )
 
+// Generates a PDF invoice based on the provided data and returns the PDF's byte data.
+//
 //encore:api public method=POST path=/generate-invoice
 func GenerateInvoice(ctx context.Context, data *model.Request) (model.Response, error) {
 	response, err := invoice.Generate(data)
