3968ac8e5f8a682df15a16855f4fa75680a6041a
Bryan Moslo <balgutria@wawand.co>
2024-09-18 09:16:43 -0500

    testing the invoice Generate method

--- go.mod
+++ go.mod
@@ -2,10 +2,14 @@ module encore.app
 
 go 1.22.2
 
-require github.com/johnfercher/maroto/v2 v2.1.2
+require (
+	github.com/johnfercher/maroto/v2 v2.1.2
+	github.com/stretchr/testify v1.8.4
+)
 
 require (
 	github.com/boombuler/barcode v1.0.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/f-amaral/go-async v0.3.0 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/hhrutter/lzw v1.0.0 // indirect
@@ -15,8 +19,10 @@ require (
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/pdfcpu/pdfcpu v0.6.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	golang.org/x/image v0.18.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ invoice/generate_test.go
@@ -0,0 +1,52 @@
+package invoice_test
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"encore.app/invoice"
+	"encore.app/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGeneratePDF(t *testing.T) {
+	request := &model.Request{
+		CustomerName:        "John Doe",
+		CustomerPhoneNumber: "(+1) 555-123-4567",
+		CustomerAddress:     "742 Evergreen Terrace, Springfield, IL 62704",
+		InvoiceNumber:       "12345",
+		InvoiceDate:         "October 1, 2024",
+		Transactions: []model.Transaction{
+			{Name: "Blue Denim Jacket", Quantity: "1", UnitPrice: "$150", Total: "$150"},
+			{Name: "Striped Polo Shirt", Quantity: "3", UnitPrice: "$80", Total: "$240"},
+			{Name: "Patterned Skirt", Quantity: "2", UnitPrice: "$65", Total: "$130"},
+		},
+		Subtotal:           "$520",
+		TaxPercentage:      "5%",
+		TaxAmount:          "$26.00",
+		Total:              "$546.00",
+		BankName:           "First National Bank",
+		OwnerAccountName:   "John Smith",
+		OwnerAccountNumber: "123-456-789-10",
+		PaymentDate:        "October 10, 2024",
+		OwnerName:          "John Smith",
+		OwnerAddress:       "123 Maple St, Anytown, CA 90210",
+	}
+
+	response, err := invoice.Generate(request)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, response.Data, "PDF data should not be empty")
+
+	outputFolder := "pdf"
+	outputFile := filepath.Join(outputFolder, fmt.Sprintf("invoice_%s.pdf", request.InvoiceNumber))
+
+	// Create the folder if it doesn't exist
+	err = os.MkdirAll(outputFolder, os.ModePerm)
+	assert.NoError(t, err)
+
+	err = os.WriteFile(outputFile, response.Data, 0644)
+	assert.NoError(t, err)
+}
